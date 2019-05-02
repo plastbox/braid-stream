b0f8273f8d4bee44f27e3a6d444c7a63c1baedee
Kim Helberg <plastbox@hotmail.com>
2019-05-02 14:44:56 +0200

    Implemented option "autoDestroy". (Default) false: braid-stream reads until all sources are exhausted, removing each source when it emits close/end. True: braid-stream destroys all source streams if one source closes/ends.

--- braid-stream.js
+++ braid-stream.js
@@ -3,10 +3,19 @@
 
 const { Readable } = require('stream');
 
-module.exports = function braid(sources) {
+module.exports = function braid(sources, options={}) {
 	if(!(sources instanceof Array)) {
 		sources = Array.prototype.slice.call(arguments, 0);
+		if(!sources[sources.length - 1]._readableState) {
+			options = sources.pop();
+		}
+		else {
+			options = {
+				autoDestroy: false
+			};
+		}
 	}
+	//console.log(`[BraidStream] :: options`, options);
 	var isDestroyed = false;
 	var braidStreamOut = new Readable({
 		highWaterMark: 1,
@@ -14,6 +23,7 @@ module.exports = function braid(sources) {
 			return doPushFromQueue();
 		},
 		destroy(err, callback) {
+			//console.log(`[BraidStream] :: destroy`);
 			if(!isDestroyed) {
 				isDestroyed = true;
 				queues = sources.map(source => {
@@ -44,11 +54,31 @@ module.exports = function braid(sources) {
 			doPushFromQueue();
 		}.bind(source));
 
-		source.on('end', () => braidStreamOut.destroy());
-		source.on('close', () => braidStreamOut.destroy());
+
+		source.on('end', removeSource.bind(this, source, 'end'));
+		source.on('close', removeSource.bind(this, source, 'close'));
 
 		return null;
 	});
+
+	function removeSource(source, eventName) {
+		var idx = sources.indexOf(source);
+		//console.log(`[BraidStream] :: removeSource ${eventName} ${idx}`);
+		if(options.autoDestroy) {
+			braidStreamOut.destroy();
+		}
+		else {
+			//console.log(`[BraidStream] :: source end ${idx}, remove source`);
+			sources.splice(idx, 1);
+			queues.splice(idx, 1);
+			if(sources.length === 0) {
+				braidStreamOut.destroy();
+			}
+			else {
+				doPushFromQueue();
+			}
+		}
+	}
 	
 	return braidStreamOut;
 };
--- test.js
+++ test.js
@@ -15,21 +15,46 @@ class RandomIntervalSource extends Readable {
 			}, options);
 		this.closeAfter = options.closeAfter;
 		this.label = options.label;
+		this.pushTimer = null;
 	}
 	_read(size) {
-		setTimeout(() => {
-			this.counter = (this.counter || 0) + 1;
-			this.push(`${(new Date()).toISOString()} source: ${this.label}`);
-			if(this.closeAfter && this.counter === this.closeAfter) {
-				this.push(null);
+		var id = Math.floor(Math.random() * 8999) + 1000;
+		//console.log('---------------------------- READ', id, this.label, this.counter, this.readable);
+		(function(id) {
+			if(this.closeAfter) {
+				if(this.counter === this.closeAfter) {
+					console.log('----------------------------CLOSE', id, this.label, this.counter, this.readable);
+					this.push(null);
+				}
 			}
-		}, this.intervalMin + Math.floor(Math.random() * this.intervalMax));
+			if(!this.closeAfter || (this.counter || 0) < this.closeAfter) {
+				//console.log('---------------------------- TIME', id, this.label, this.counter, this.readable);
+				clearTimeout(this.pushTimer);
+				this.pushTimer = setTimeout(() => {
+					//console.log('---------------------------- PUSH', id, this.label, this.counter, this.readable);
+					this.counter = (this.counter || 0) + 1;
+					try {
+						this.push(`${(new Date()).toISOString()} source: ${this.label}`);
+					} catch(e) {
+						console.log('----------------------------ERROR', id, this.label, this.counter, this.readable);
+						throw(e);
+					}
+					
+				}, this.intervalMin + Math.floor(Math.random() * this.intervalMax));
+			}
+		}).call(this, id);	
+	}
+	_destroy(err, callback) {
+		console.log('--------------------------DESTROY', err, this.label, this.counter, this.readable);
+		this.push(null);
+		clearTimeout(this.pushTimer);
+		callback(err);
 	}
 }
-var source1 = new RandomIntervalSource({label: '1', closeAfter: 100000});
-var source2 = new RandomIntervalSource({label: '2', });
-var source3 = new RandomIntervalSource({label: '3', });
-var source4 = new RandomIntervalSource({label: '4', });
+var source1 = new RandomIntervalSource({label: '1', closeAfter: 40});
+var source2 = new RandomIntervalSource({label: '2', closeAfter: 30});
+var source3 = new RandomIntervalSource({label: '3', closeAfter: 20});
+var source4 = new RandomIntervalSource({label: '4', closeAfter: 10});
 
 var sink = new Writable({
 	write: function(chunk, encoding, next) {
@@ -44,4 +69,4 @@ var sink = new Writable({
 	}
 });
 //braid([source1, source2, source3]);
-braid(source1, source2, source3, source4).pipe(sink);
+braid(source1, source2, source3, source4, {autoDestroy: true}).pipe(sink);
